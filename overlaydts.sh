3342dd20bbce0a90f8747ed46761e297f944849d
shrkey <team@shrkey.com>
2014-01-27 22:58:33 +0000

    Made pwm names more generic

--- overlaydts.sh
+++ overlaydts.sh
@@ -4,3 +4,8 @@ export SLOTS=/sys/devices/bone_capemgr.9/slots
 export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
 
 echo "Copying dts files - "
+
+sudo su
+cd /lib/firmware
+echo am33xx_pwm > $SLOTS
+echo cape-bone-julius > $SLOTS
